5e0c511a51edbb9f6d11c0165a2eb2787367f183
Votre Nom <HSPH9696@orange.com>
2025-02-13 09:50:57 +0100

    Ajout des fichiers Python

---
+++ test_api.py
@@ -0,0 +1,55 @@
+from fastapi.testclient import TestClient
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
+from api import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == 'Welcome to the API'
+
+
+
+from fastapi import status
+import requests
+import json
+
+
+
+def test_check_client_id():
+    """Test la fonction check_client_id() de l'API avec un client faisant partie de la base de données."""
+    url = API_URL + str(192535)
+    response = requests.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert json.loads(response.content) == True
+
+
+def test_check_client_id_2():
+    """Test la fonction check_client_id() de l'API avec un client ne faisant pas partie de la base de données."""
+    url = API_URL + str(100000)
+    response = requests.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert json.loads(response.content) == False
+
+
+def test_get_prediction():
+    """Test la fonction get_prediction() de l'API."""
+    url = API_URL + "prediction/" + str(192535)
+    response = requests.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert json.loads(response.content) == 0.4805479971101088
+
+
+import unittest
+
+class TestExample(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
+
